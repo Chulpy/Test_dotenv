a28d33a77308c9942bce8c5ef5efebb1d148ea65
Chulpy <tchoulpan@gmail.com>
2022-01-26 15:10:50 +0100

    Commit #5

--- lib/app.rb
+++ lib/app.rb
@@ -7,14 +7,15 @@ Dotenv.load('.env') # Ceci appelle le fichier .env (situé dans le même dossier
 
 # quelques lignes qui appellent les clés d'API de ton fichier .env
 
+def login_twitter
   client = Twitter::REST::Client.new do |config|
     config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
     config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
     config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
     config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
     return client
-    binding.pry
   end
 
 # ligne qui permet de tweeter sur ton compte
 client.update('Here is my first tweet, yey.')
+end
